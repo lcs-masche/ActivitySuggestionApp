d813bdf4f821eb0bc25f58381944b19c39f2ca22
Mo Asche <masche@lcs.on.ca>
2021-10-11 21:43:44 -0400

    added the link, that the code will receive the data from and I added a structure, that identifies each part of the reciving data to be able to use/decode it

---
+++ ActivitySuggestionApp/Data/activitySuggestion.swift
@@ -0,0 +1,17 @@
+//
+//  activitySuggestion.swift
+//  ActivitySuggestionApp
+//
+//  Created by Moritz Conrad Asche on 2021-10-11.
+//
+
+import Foundation
+
+struct suggestedActivity: Decodable {
+    let activity: String
+    let type: String
+    let participants: Int
+    let price: Double
+    let link: String
+    let accessibility: String
+}
---
+++ ActivitySuggestionApp/Data/recievingAPI.swift
@@ -0,0 +1,12 @@
+//
+//  recievingAPI.swift
+//  ActivitySuggestionApp
+//
+//  Created by Moritz Conrad Asche on 2021-10-11.
+//
+
+import Foundation
+
+enum APIData {
+    static let URL = "https://www.boredapi.com/api/activity"
+}
